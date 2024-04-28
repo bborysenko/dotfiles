5a6707ed2fc62267c8d23b98f79f9e18c0691955
Borys Borysenko <3684918+bborysenko@users.noreply.github.com>
2024-04-28 03:36:40 +0300

    Install OrbStack

---
+++ run_onchange_before_005-install-orbstack.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$CHEZMOI_OS" != "darwin" ]]; then exit; fi
+
+if ! brew list orbstack &> /dev/null; then
+	echo "⏳ Installing OrbStack ..."
+	brew install --cask orbstack
+	echo "✅ OrbStack has been installed"
+else
+	echo "✅ OrbStack has been already installed"
+fi
