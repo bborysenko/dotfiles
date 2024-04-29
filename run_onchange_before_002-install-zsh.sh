0c2ddfa1662da810e92914b7e4e10a00b1c4e918
Borys Borysenko <3684918+bborysenko@users.noreply.github.com>
2024-04-29 14:54:36 +0300

    Install Zsh on Linux

--- run_onchange_before_002-install-zsh.sh
+++ run_onchange_before_002-install-zsh.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+if [[ "$CHEZMOI_OS" == "linux" ]]; then
+	if ! zsh --version &> /dev/null; then
+		echo "⏳ Installing Zsh ..."
+		sudo apt-get update
+		sudo apt-get install zsh
+		sudo chsh -s $(which zsh) $(whoami)
+		echo "✅ Zsh has been installed"
+	else
+		echo "✅ Zsh has been already installed"
+	fi
+fi
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "⏳ Installing Oh My Zsh ..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
