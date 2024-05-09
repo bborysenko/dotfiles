10c3ff7c01d5f03b19571493256bd09363cea361
Borys Borysenko <3684918+bborysenko@users.noreply.github.com>
2024-05-09 16:24:23 +0300

    Install Terraform with Asdf

---
+++ run_onchange_before_009-install-terraform.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$CHEZMOI_OS" != "darwin" ]]; then exit; fi
+
+if ! terraform version &> /dev/null; then
+	echo "⏳ Installing Terraform ..."
+	asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+	asdf install terraform latest
+	asdf global terraform latest
+	echo "✅ Terraform has been installed"
+else
+	echo "✅ Terraform has been already installed"
+fi
