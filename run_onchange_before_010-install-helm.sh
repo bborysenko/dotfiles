fa5af479755bd8375bd013c40703c56aa68c2fc3
Borys Borysenko <3684918+bborysenko@users.noreply.github.com>
2024-05-23 15:11:02 +0300

    Install Helm plugins

--- run_onchange_before_010-install-helm.sh
+++ run_onchange_before_010-install-helm.sh
@@ -13,3 +13,20 @@ if ! helm version &> /dev/null; then
 else
 	echo "✅ Helm has been already installed"
 fi
+
+plugins=(
+	"diff https://github.com/databus23/helm-diff"
+)
+
+for plugin in "${plugins[@]}"; do
+	plugin_name=$(echo "$plugin" | awk '{print $1}')
+	plugin_repo=$(echo "$plugin" | awk '{print $2}')
+
+	if ! helm plugin list | grep -q "^$plugin_name"; then
+		echo "⏳ Installing Helm plugin $plugin_name ..."
+		helm plugin install "$plugin_repo"
+		echo "✅ Helm plugin $plugin_name has been installed"
+	else
+		echo "✅ Helm plugin $plugin_name is already installed"
+	fi
+done
